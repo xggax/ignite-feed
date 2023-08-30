df565a7629944883859ca1d26aff7f8cb2d89c8c
Gabriel Gama <gabrielgamagga@gmail.com>
2023-08-30 11:18:52 -0400

    feat: Criando projeto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Post } from "./Post"
+
+export function App() {
+  return (
+    <>
+      <Post 
+        author="John"
+        content="EXEMPLO CONTENT"
+      />
+      <Post 
+        author="Edward"
+        content="EXEMPLO CONTENT 2"
+      />
+    </>
+  )
+}
