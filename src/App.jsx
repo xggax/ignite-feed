2205e1d921651a639a9745f06aa735c40c4fd68d
Gabriel Gama <gabrielgamagga@gmail.com>
2023-08-30 16:06:25 -0400

    feat: adiciona sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import { Post } from "./Post"
 import { Header } from "./components/Header";
 
+import styles from './App.module.css';
 import './global.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <>
       <Header />
-      <Post 
-        author="John"
-        content="EXEMPLO CONTENT"
-      />
-      <Post 
-        author="Edward"
-        content="EXEMPLO CONTENT 2"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="John"
+            content="EXEMPLO CONTENT"
+          />
+          <Post 
+            author="Edward"
+            content="EXEMPLO CONTENT 2"
+          />
+        </main>
+      </div>
     </>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react';
+export function Sidebar(){
+  return(
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover} 
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+      />
+      
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/12355794?v=4"/>
+
+        <strong>Gabriel Gama</strong>
+        <span>Web developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/> 
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
