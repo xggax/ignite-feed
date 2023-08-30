87078c9f9ad7836396c6e0c80f628472b4109db0
Gabriel Gama <gabrielgamagga@gmail.com>
2023-08-30 14:37:22 -0400

    feat: adiciona componente header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import styles from './Header.module.css';
 
+import igniteLogo from "../assets/ignite-logo.svg"; 
+
 export function Header(){
   return(
     <header>
       <strong className={styles.header}>
-        Ignite Feed
+        <img src={igniteLogo} alt="Logotipo do Ignite"/>
       </strong>
     </header>
   );
