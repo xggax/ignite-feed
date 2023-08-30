fe9a5cddda90ed26833f141cf2f0fa3cd15c567d
Gabriel Gama <gabrielgamagga@gmail.com>
2023-08-30 13:53:42 -0400

    feat: adiciona css estilos globais

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Post } from "./Post"
+import { Header } from "./components/Header";
+
+import './global.css';
 
 export function App() {
   return (
     <>
+      <Header />
       <Post 
         author="John"
         content="EXEMPLO CONTENT"
