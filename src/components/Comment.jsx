81a9a8a0cb267b0c3063bfc4e5163d96ddbc6a92
Gabriel Gama <gabrielgamagga@gmail.com>
2023-09-01 16:58:25 -0400

    feat: adiciona comportamentos de likes, comentarios, post e formatacao de datas

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,21 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
-export function Comment(){
+export function Comment({ content, onDeleteComment }){
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment(){
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment(){
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/xggax.png" alt="" />
@@ -14,17 +27,17 @@ export function Comment(){
               <time title="31 de Agosto de 2023 às 11:35h" dateTime="2023-08-31 11:35:00">Cerca de 1h atrás</time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={ handleDeleteComment} title="Deletar comentário">
               <Trash size={20} />
             </button>
           </header>
-          <p>Muito Bom, parabéns!! </p>
+            <p>{ content }</p>
         </div>
 
         <footer>
-          <button>
+          <button onClick={ handleLikeComment }>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,48 +1,105 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBr from 'date-fns/locale/pt-BR';
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
+import { useState } from 'react';
 
-export function Post(){
+export function Post({ author, publishedAt, content }){
+  const [comments, setComments] = useState(['Post muito legal, hein!?']);
+  const [newCommentText, setNewCommentText] = useState(['']);
+
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'de' yyyy 'às' HH:mm'h'", { locale: ptBr });
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, { 
+    locale: ptBr,
+    addSuffix: true
+  });
+
+  function handleCreateNewComment(){
+    event.preventDefault();
+
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  }
+
+  function handleNewCommentChange(){
+    event.target.setCustomValidity('');
+    setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid(){
+    event.target.setCustomValidity('Esse campo é obrigatório!');
+  }
+
+  function deleteComment(commentToDelete){
+    const commentsWithoutDeletedOne = comments.filter(comment => {
+      return comment !== commentToDelete;
+    });
+    
+    setComments(commentsWithoutDeletedOne);
+  }
+
+  const isNewCommentIsEmpty = newCommentText.length === 0;
+
   return(
     <article className={styles.post}>
        <header>
           <div className={styles.author}>
-            <Avatar src="https://github.com/xggax.png" />
+            <Avatar src={author.avatarUrl} />
             <div className={styles.authorInfo}>
-              <strong>Gabriel Gama</strong>
-              <span>Web Developer</span>
+              <strong>{author.name}</strong>
+              <span>{author.role}</span>
             </div>
           </div>
 
-          <time title="31 de Agosto de 2023 às 11:35h" dateTime="2023-08-31 11:35:00">Publicado há 1h</time>
+          <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+            { publishedDateRelativeToNow }
+          </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p><a href=''>jane.design/doctorcare</a></p>
-        <p>
-          <a href=''>#novoprojeto</a>{' '}
-          <a href=''>#nlw</a>{' '}
-          <a href=''>#rocketseat</a>{' '} 
-        </p>
+        {content.map(line => {
+            if(line.type === "paragraph"){
+              return <p key={line.content}>{ line.content }</p>;
+            }else if (line.type === "link"){
+              return <p key={line.content}><a href="#">{line.content}</a></p>; 
+            }
+          })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
-        <textarea 
+        <textarea
+          name="comment"
           placeholder='Deixe seu comentário'
+          onChange={handleNewCommentChange}
+          value={newCommentText}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type='submit'>Publicar</button>
+          <button 
+            type='submit' 
+            disabled={isNewCommentIsEmpty}
+          >
+            Publicar
+          </button>
         </footer>
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {
+          comments.map((comment, index) => {
+            return (
+              <Comment 
+                key={ comment } 
+                content={ comment } 
+                onDeleteComment={ deleteComment }/>
+            )
+          })
+        }
       </div>
     </article>
   )
